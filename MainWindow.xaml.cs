efbbf7c5d1720aef2bc36615ff01e3bff09d0b9a
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2015-12-15 15:54:05 +0100

    WIP WPF transparenchy

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Svg;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace jimney
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string resourceName = "jimney.Resources.Shapes.svg";
+            using (Stream readStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            using (MemoryStream writeStream = new MemoryStream())
+            {
+                SvgDocument svgDocument = SvgDocument.Open<SvgDocument>(readStream);
+
+                BitmapImage bitmapImage = new BitmapImage();
+                svgDocument.Draw().Save(writeStream, ImageFormat.Png);
+                writeStream.Seek(0, SeekOrigin.Begin);
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = writeStream;
+                bitmapImage.EndInit();
+
+                imageMain.Width = (int)svgDocument.Width;
+                imageMain.Height = (int)svgDocument.Height;
+                imageMain.Source = bitmapImage;
+
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Close();
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
